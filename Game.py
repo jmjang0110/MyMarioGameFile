1623a7e0ff82f0b5a4f861b10bc9df30c72b91fe
jmjang0110 <90159618+jmjang0110@users.noreply.github.com>
2021-10-20 14:09:54 +0900

    Mario Add Direction
    Direction LEFT / RIGHT

--- Game.py
+++ Game.py
@@ -3,18 +3,22 @@ import random
 import math
 from MarioClass import *
 
-
-
-
-
 def handle_events():
     global running
+    global mario1
+
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_RIGHT:
+            mario1.ChangeDirection(Direction.RIGHT)
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_LEFT:
+            mario1.ChangeDirection(Direction.LEFT)
+
+
 
 open_canvas()
 
--- MarioClass.py
+++ MarioClass.py
@@ -17,20 +17,48 @@ class Mario:
         self.image_right = load_image('mario_right.png')
         self.image_left = load_image('mario_left.png')
         self.image_Width = 100
-        self.image_Height = 98
+        self.image_Height = 100
+
+        self.Character_Width = 100
+        self.Character_Height = 98
 
         self.direction = Direction.RIGHT
         self.x, self.y = random.randint(50,400), 90
+        self.dst = 1
         self.frame = 0
 
     def update(self):
-        self.x += 5
-        if self.x >= 700:
-            self.x = 700
+        self.x += (5 * self.dst)
+        if self.x >= 800:
+            self.x = 800
+        elif self.x <= 0:
+            self.x = 0
 
-        self.frame = (self.frame + 1) % 3
+
+        self.frame = (self.frame + 1) % 2
+
+    #   캐릭터의 방향을 바꿉니다.
+    def ChangeDirection(self, NewDirection):
+        self.direction = NewDirection
+        self.UpdateDst()
+
+
+    # 방향에 따른 좌표 이동 방향을 바꿉니다.
+    def UpdateDst(self):
+        if self.direction == Direction.RIGHT:
+            self.dst = 1
+        elif self.direction == Direction.LEFT:
+            self.dst = -1
+
 
 
     def draw(self):
         if self.direction == Direction.RIGHT:
-            self.image_right.clip_draw(self.frame * self.image_Width,self.image_Height, 100,100, self.x, self.y)
+            self.image_right.clip_draw(300 + self.frame * self.image_Width, self.image_Height * 3,
+                                       self.Character_Width, self.Character_Height, self.x, self.y)
+        elif self.direction == Direction.LEFT:
+            self.image_left.clip_draw(200 + self.frame * self.image_Width, self.image_Height,
+                                      self.Character_Width, self.Character_Height, self.x, self.y)
+        elif self.direction == Direction.STOP:
+            self.image_right.clip_draw(self.frame * self.image_Width,self.image_Height,
+                                       self.Character_Width, self.Character_Height, self.x, self.y)
