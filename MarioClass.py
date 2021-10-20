6f41edcea5ec759e0447059d3c45952bcaf3281a
jmjang0110 <90159618+jmjang0110@users.noreply.github.com>
2021-10-20 22:23:13 +0900

    add BackGround for Test
    Add BackGround with Mario
    just for Test

--- MarioClass.py
+++ MarioClass.py
@@ -15,11 +15,9 @@ WINDOW_SIZE_WIDTH = 800
 WINDOW_SIZE_HEIGHT = 600
 
 
-
-
 class Mario:
     def __init__(self):
-
+        self.BackGround_forTest = load_image('Mario_BackGround_Test.png')
         self.image_right = load_image('mario_right.png')
         self.image_left = load_image('mario_left.png')
         self.image_WIDTH = 100
@@ -28,42 +26,39 @@ class Mario:
         self.direction = Direction.RIGHT
         self.jumpdirection = Direction.UP
 
-        self.x, self.y = random.randint(50,400), 90
+        self.x, self.y = random.randint(50,400), 120
         self.dst = 1
         self.frame, self.frame_dst = 0, 1
 
         # 점프를 위한 변수
         self.isJump = False
         self.jumpX = 10.0
-        self.jumpY = 90.0
+        self.jumpY = 120.0
         self.diameter = 10.0
 
         self.jumpTime = 0.0
         self.jumpHeight = 0.0
         self.jumpPower = 50.0  # 이 값을 높이면 더 높이 점프 할 수 있습니다.
         self.jumpSpeed = 4.0   # 이 값을 높이면 점프하는 속도가 빨라집니다..
-
-        self.Velocity = 0.0
-        self.Gravity = 400.0
-        self.posY = 0.0
+        self.posY = 0.0        # 마리오 점프 시작 위치
 
 
     def Jump(self):
         self.jumpHeight = (self.jumpTime * self.jumpTime - self.jumpPower * self.jumpTime) / 4.0
         self.jumpTime += self.jumpSpeed
-        # 마리오 : 점프에 따른 y값 조정 
+        # 마리오 : 점프에 따른 y값 조정
         self.y = self.posY + self.jumpHeight * -1
 
-        if self.jumpTime >= self.jumpPower / 2:
+        if self.jumpTime >= self.jumpPower / 5 * 4:
             self.jumpdirection = Direction.DOWN
 
         print(self.jumpTime , '  ',  self.jumpPower)
 
-        if self.y < 90:
+        if self.y < 120:
             self.jumpTime = 0
             self.jumpHeight = 0
             self.isJump = False
-            self.y = 90
+            self.y = 120
             self.jumpdirection = Direction.UP
 
         # if self.jumpTime > self.jumpPower:
@@ -101,6 +96,10 @@ class Mario:
 
 
     def draw(self):
+
+        # 배경화면 그리기 ( 테스트 입니다.. )
+        self.BackGround_forTest.clip_draw(0,0,600,385,400,300,WINDOW_SIZE_WIDTH,WINDOW_SIZE_HEIGHT)
+
         # 마리오 : 오른쪽 / 점프 / 위로
         if self.isJump == True and self.direction == Direction.RIGHT and self.jumpdirection == Direction.UP:
             self.image_right.clip_draw(0, self.image_HEIGHT * 2,
